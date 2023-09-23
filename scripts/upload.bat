0f4fc4d7db346fb8ca6e0f2896c48bd73705584f
Artiom Kenibasov <artiom@gmail.com>
2023-09-23 20:09:45 +0200

    Upload artfacts when building

--- scripts/upload.bat
+++ scripts/upload.bat
@@ -4,4 +4,5 @@ C:\Users\User\.platformio\penv\Scripts\python.exe "C:\Users\User\.platformio\pac
     0x1000 C:\Users\User\Documents\PlatformIO\Projects\NSClock\.pio\build\ulanzi\bootloader.bin ^
     0x8000 C:\Users\User\Documents\PlatformIO\Projects\NSClock\.pio\build\ulanzi\partitions.bin ^
     0xe000 C:\Users\User\.platformio\packages\framework-arduinoespressif32\tools\partitions\boot_app0.bin ^
-    0x10000 C:\Users\User\Documents\PlatformIO\Projects\NSClock\.pio\build\ulanzi\firmware.bin
+    0x10000 C:\Users\User\Documents\PlatformIO\Projects\NSClock\.pio\build\ulanzi\firmware.bin ^
+    0x210000 C:\Users\User\Documents\PlatformIO\Projects\NSClock\.pio\build\ulanzi\littlefs.bin
