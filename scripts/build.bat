638e25f0370340205ffddca114336ee1df8747ad
Artiom Kenibasov <artiom@gmail.com>
2023-09-24 21:09:43 +0200

    More supporting scripts

---
+++ scripts/build.bat
@@ -0,0 +1,3 @@
+cd ..
+C:\Users\User\.platformio\penv\Scripts\platformio.exe run --environment ulanzi
+cd Scripts
--- scripts/upload.bat
+++ scripts/upload.bat
@@ -1,8 +1,4 @@
 C:\Users\User\.platformio\penv\Scripts\python.exe "C:\Users\User\.platformio\packages\tool-esptoolpy\esptool.py" ^
     --chip esp32 --port "COM6" --baud 921600 --before default_reset --after hard_reset write_flash -z ^
     --flash_mode dio --flash_freq 40m --flash_size 4MB ^
-    0x1000 C:\Users\User\Documents\PlatformIO\Projects\NSClock\.pio\build\ulanzi\bootloader.bin ^
-    0x8000 C:\Users\User\Documents\PlatformIO\Projects\NSClock\.pio\build\ulanzi\partitions.bin ^
-    0xe000 C:\Users\User\.platformio\packages\framework-arduinoespressif32\tools\partitions\boot_app0.bin ^
-    0x10000 C:\Users\User\Documents\PlatformIO\Projects\NSClock\.pio\build\ulanzi\firmware.bin ^
-    0x210000 C:\Users\User\Documents\PlatformIO\Projects\NSClock\.pio\build\ulanzi\littlefs.bin
+    0x10000 C:\Users\User\Documents\PlatformIO\Projects\NSClock\.pio\build\ulanzi\firmware.bin
