746df572f9c3844edea52c59dec04fc0e99cf8d9
Artiom Kenibasov <artiom@gmail.com>
2023-10-15 13:33:08 +0200

    Added retry logic to upload

--- scripts/upload.bat
+++ scripts/upload.bat
@@ -1,4 +1,12 @@
-C:\Users\User\.platformio\penv\Scripts\python.exe "C:\Users\User\.platformio\packages\tool-esptoolpy\esptool.py" ^
-    --chip esp32 --port "COM6" --baud 921600 --before default_reset --after hard_reset write_flash -z ^
-    --flash_mode qio --flash_freq 40m --flash_size 4MB ^
-    0x10000 C:\Users\User\Documents\PlatformIO\Projects\NSClock\.pio\build\ulanzi\firmware.bin
+@echo off
+:retry
+    REM Upload firmware
+    C:\Users\User\.platformio\penv\Scripts\python.exe "C:\Users\User\.platformio\packages\tool-esptoolpy\esptool.py" ^
+        --chip esp32 --port "COM6" --baud 921600 --before default_reset --after hard_reset write_flash -z ^
+        --flash_mode qio --flash_freq 40m --flash_size 4MB ^
+        0x10000 C:\Users\User\Documents\PlatformIO\Projects\NSClock\.pio\build\ulanzi\firmware.bin
+    REM Check result
+    if errorlevel 1 goto retry
+    REM Upload success
+    echo "Upload success"
+    goto end
