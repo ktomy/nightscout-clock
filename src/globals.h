15f4becb9761a88596b9517c48172c4738a2d91c
Artiom Kenibasov <artiom@gmail.com>
2024-03-24 01:27:07 +0100

    Bump version to 0.7

--- src/globals.h
+++ src/globals.h
@@ -3,7 +3,7 @@
 #include <Arduino.h>
 #include <IPAddress.h>
 
-#define VERSION "0.6"
+#define VERSION "0.7"
 
 #ifdef DEBUG
 #define DEBUG_PRINTLN(x)                                                                                                         \
