e4f4729ca3fae625e6f03131533ec0ffc4ff0d64
Artiom Kenibasov <artiom@gmail.com>
2024-11-10 00:39:33 +0100

    Add debug alarms feature and adjust snooze delay

--- src/BGAlarmManager.cpp
+++ src/BGAlarmManager.cpp
@@ -5,6 +5,8 @@
 #include "ServerManager.h"
 #include <PeripheryManager.h>
 
+#define ALARM_REPEAT_INTERVAL_SECONDS 300
+
 // The getter for the instantiated singleton instance
 BGAlarmManager_ &BGAlarmManager_::getInstance() {
     static BGAlarmManager_ instance;
@@ -13,6 +15,18 @@ BGAlarmManager_ &BGAlarmManager_::getInstance() {
 
 // Initialize the global shared instance
 BGAlarmManager_ &bgAlarmManager = bgAlarmManager.getInstance();
+
+#ifdef DEBUG_ALARMS
+
+int debounceTicks = 0;
+int debounceTicks2 = 0;
+int debounceTicks3 = 0;
+int debounceTicks4 = 0;
+int debounceTicks5 = 0;
+int debounceTicks6 = 0;
+int debounceTicks7 = 0;
+
+#endif
 
 void BGAlarmManager_::setup() {
     if (SettingsManager.settings.alarm_urgent_low_enabled) {
@@ -48,35 +62,99 @@ void BGAlarmManager_::setup() {
 }
 
 bool isInSilentInterval(String silenceInterval) {
-    // TODO: Implement this function
     if (silenceInterval == "" || silenceInterval == "0") {
+#ifdef DEBUG_ALARMS
+
+        if (debounceTicks3 % 5000 == 0) {
+            DEBUG_PRINTLN("Alarms: no silence interval defined");
+        }
+        debounceTicks3++;
+        if (debounceTicks3 > 5000) {
+            debounceTicks3 = 0;
+        }
+
+#endif        
         return false;
     }
-    if (silenceInterval == "22_8") {
-        auto time = ServerManager.getTimezonedTime();
-        if (time.tm_hour >= 22 || time.tm_hour < 8) {
-            return true;
-        }
-    }
-    if (silenceInterval == "8_22") {
-        auto time = ServerManager.getTimezonedTime();
-        if (time.tm_hour >= 8 && time.tm_hour < 22) {
-            return true;
-        }
+
+    auto time = ServerManager.getTimezonedTime();
+
+    if (silenceInterval == "22_8" && (time.tm_hour >= 22 || time.tm_hour < 8)) {
+#ifdef DEBUG_ALARMS
+    
+            if (debounceTicks4 % 5000 == 0) {
+                DEBUG_PRINTLN("Alarms: silence interval 22_8 active");
+            }
+            debounceTicks4++;
+            if (debounceTicks4 > 5000) {
+                debounceTicks4 = 0;
+            }   
+#endif
+        return true;
     }
 
+    if (silenceInterval == "8_22" && (time.tm_hour >= 8 && time.tm_hour < 22)) {
+#ifdef DEBUG_ALARMS
+        
+                if (debounceTicks5 % 5000 == 0) {
+                    DEBUG_PRINTLN("Alarms: silence interval 8_22 active");
+                }
+                debounceTicks5++;
+                if (debounceTicks5 > 5000) {
+                    debounceTicks5 = 0;
+                }
+#endif        
+        return true;
+    }
+
+#ifdef DEBUG_ALARMS
+        
+    if (debounceTicks6 % 5000 == 0) {
+        DEBUG_PRINTLN("Alarms: silence interval exists but not active");
+    }
+    debounceTicks6++;
+    if (debounceTicks6 > 5000) {
+        debounceTicks6 = 0;
+    }
+#endif
     return false;
 }
+
+#ifdef DEBUG_ALARMS
+#endif
 
 void BGAlarmManager_::tick() {
     auto glucoseReading = bgDisplayManager.getLastDisplayedGlucoseReading();
     if (glucoseReading == nullptr || glucoseReading->getSecondsAgo() > BG_DATA_OLD_OFFSET_MINUTES * 60) {
         activeAlarm = NULL;
+
+#ifdef DEBUG_ALARMS
+    
+            if (debounceTicks % 5000 == 0) {
+                DEBUG_PRINTLN("Alarms: no alarms as no glucose readings or readings are old");
+            }
+            debounceTicks++;
+            if (debounceTicks > 5000) {
+                debounceTicks = 0;
+            }
+#endif
         return;
     }
 
     for (AlarmData &alarmData : enabledAlarms) {
         if (glucoseReading->sgv >= alarmData.bottom && glucoseReading->sgv <= alarmData.top) {
+#ifdef DEBUG_ALARMS
+    
+                if (debounceTicks2 % 5000 == 0) {
+                    DEBUG_PRINTLN("Alarms: glucose reading in alarm range");
+                }
+                debounceTicks2++;
+                if (debounceTicks2 > 5000) {
+                    debounceTicks2 = 0;
+                }
+
+#endif
+
             if (isInSilentInterval(alarmData.silenceInterval)) {
                 if (activeAlarm != NULL) {
                     activeAlarm->isSnoozed = false;
@@ -89,21 +167,46 @@ void BGAlarmManager_::tick() {
             if (activeAlarm == NULL) {
                 activeAlarm = &alarmData;
                 alarmData.lastAlarmTime = ServerManager.getUtcEpoch();
+                alarmData.isSnoozed = false;
                 PeripheryManager.playRTTTLString(alarmData.alarmSound);
-                DEBUG_PRINTLN("Playing alarm sound");
+                DEBUG_PRINTLN("Playing alarm sound (nee alarm occurred)");
             } else {
                 if (activeAlarm->isSnoozed) {
                     if (activeAlarm->snoozeTimeMinutes != 0 && ServerManager.getUtcEpoch() - activeAlarm->lastAlarmTime > 60 * activeAlarm->snoozeTimeMinutes) {
                         activeAlarm->isSnoozed = false;
                         activeAlarm->lastAlarmTime = ServerManager.getUtcEpoch();
                         PeripheryManager.playRTTTLString(alarmData.alarmSound);
-                        DEBUG_PRINTLN("Playing alarm sound");
+                        DEBUG_PRINTLN("Playing alarm sound after snooze");
+                    }
+                    else {
+#ifdef DEBUG_ALARMS
+
+                        if (debounceTicks8 % 5000 == 0) {
+                            DEBUG_PRINTLN("Alarms: snoozed, too early to sound: " + String(ServerManager.getUtcEpoch() - activeAlarm->lastAlarmTime) + ", snooze interval: " + String(activeAlarm->snoozeTimeMinutes));
+                        }
+                        debounceTicks8++;
+                        if (debounceTicks8 > 5000) {
+                            debounceTicks8 = 0;
+                        }
+#endif
                     }
                 } else {
-                    if (ServerManager.getUtcEpoch() - activeAlarm->lastAlarmTime > 60 * 5) {
+                    if (ServerManager.getUtcEpoch() - activeAlarm->lastAlarmTime > ALARM_REPEAT_INTERVAL_SECONDS) {
                         activeAlarm->lastAlarmTime = ServerManager.getUtcEpoch();
                         PeripheryManager.playRTTTLString(alarmData.alarmSound);
-                        DEBUG_PRINTLN("Playing alarm sound");
+                        DEBUG_PRINTLN("Playing alarm sound (alarm already active, not snoozed)");
+                    }
+                    else {
+#ifdef DEBUG_ALARMS
+
+                        if (debounceTicks7 % 5000 == 0) {
+                            DEBUG_PRINTLN("Alarms: not snoozed, too early to sound: " + String(ServerManager.getUtcEpoch() - activeAlarm->lastAlarmTime));
+                        }
+                        debounceTicks7++;
+                        if (debounceTicks7 > 5000) {
+                            debounceTicks7 = 0;
+                        }
+#endif
                     }
                 }
             }
@@ -123,7 +226,7 @@ void BGAlarmManager_::snoozeAlarm() {
         DisplayManager.clearMatrix();
         DisplayManager.setTextColor(COLOR_CYAN);
         DisplayManager.printText(0, 6, "Snoozed", TEXT_ALIGNMENT::CENTER, 0);
-        delay(1000);
+        delay(2000);
         bgDisplayManager.maybeRrefreshScreen(true);
         activeAlarm->isSnoozed = true;
     }
