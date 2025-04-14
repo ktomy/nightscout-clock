47a09c0685649bd25cb9fdd888e2664d409391b1
TheBeardedTechGuy <thebeardedtechdude@gmail.com>
2025-04-13 23:32:10 -0400

    Update globals.h
    bump version

--- src/globals.h
+++ src/globals.h
@@ -3,7 +3,7 @@
 #include <Arduino.h>
 #include <IPAddress.h>
 
-#define VERSION "0.16.1"
+#define VERSION "0.16.2"
 
 #ifdef DEBUG
 #define DEBUG_PRINTLN(x)                                                                                                         \
