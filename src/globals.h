dfd5c756baa87c266d9d16a7dfd43089756d0715
Artiom Kenibasov <artiom@gmail.com>
2025-05-27 23:10:43 +0200

    Bump version to 0.19

--- src/globals.h
+++ src/globals.h
@@ -3,7 +3,7 @@
 #include <Arduino.h>
 #include <IPAddress.h>
 
-#define VERSION "0.18"
+#define VERSION "0.19"
 
 #ifdef DEBUG
 #define DEBUG_PRINTLN(x)                                                                                                         \
