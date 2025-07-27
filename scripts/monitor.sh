61e9389826d4bdf85887f096199e918bf5fe6169
Artiom Kenibasov <artiom@gmail.com>
2025-07-27 14:52:23 +0200

    Update log file path in monitor script to ensure logs are stored in the correct directory

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -6,6 +6,6 @@ else
     PROJECTDIR=$PWD
 fi
 
-LOGFILE="$PROJECTDIR/monitor_$(date +'%Y%m%d_%H%M%S').log"
+LOGFILE="$PROJECTDIR/log/monitor_$(date +'%Y%m%d_%H%M%S').log"
 
 platformio device monitor --project-dir "$PROJECTDIR" --environment ulanzi_debug | tee "$LOGFILE"
